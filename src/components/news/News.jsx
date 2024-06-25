2e78d6e3cf1101bd5fabe4534b3ca3c626d31051
Pranav <pranavparasar321@gmail.com>
2024-06-25 12:17:49 +0530

    minor bug fix

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -3,15 +3,16 @@ import "./news.scss";
 const News = ({ articles }) => {
   return (
     <>
-      {articles.map((news) => (
-        <Card
-          key={news.url}
-          title={news.title}
-          desc={news.description}
-          src={news.urlToImage}
-          url={news.url}
-        />
-      ))}
+      {articles &&
+        articles.map((news) => (
+          <Card
+            key={news.url}
+            title={news.title}
+            desc={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))}
     </>
   );
 };
